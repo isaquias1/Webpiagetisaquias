ed54198d399851003cfd84ea950b43ea0994db4a
“isaquias1” <xboxisaquias@gmail.com>
2025-03-25 11:19:14 -0300

    Controles e migrations dbs database

--- WebpiagetisaquiasNew/Data/ApplicationDbContext.cs
+++ WebpiagetisaquiasNew/Data/ApplicationDbContext.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using WebpiagetisaquiasNew.Models;
 
 namespace WebpiagetisaquiasNew.Data;
 
@@ -9,4 +10,6 @@ public class ApplicationDbContext : IdentityDbContext
         : base(options)
     {
     }
+
+public DbSet<WebpiagetisaquiasNew.Models.Aluno> Aluno { get; set; } = default!;
 }
